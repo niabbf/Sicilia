d8f04297a38ddb1562471e37b2fa5df8c9c028a6
Fan Shi <30856132+Accelerator66@users.noreply.github.com>
2018-10-26 01:14:14 +0800

    Change card to template

--- frontend/src/components/UserInfo.vue
+++ frontend/src/components/UserInfo.vue
@@ -16,34 +16,8 @@
 </div>
 
 <div style="margin-top: 70px; padding-left: 20px; padding-right: 20px">
-        <mu-card style="width: 100%; max-width: 375px; margin: 0 auto; margin-top: 20px">
-          <mu-flex class="flex-wrapper" justify-content="start">
-            <mu-card-header title="Chen JY" sub-title="Fudan University">
-              <mu-avatar slot="avatar">
-                <img src="../assets/head.jpg">
-              </mu-avatar>
-            </mu-card-header>
-          </mu-flex>
-          <mu-flex class="flex-wrapper" justify-content="start" style="margin-left: 15px">
-            <mu-chip color="info" class="tag">find dog</mu-chip>
-            <mu-chip color="success" class="tag">Shanghai</mu-chip>
-          </mu-flex>
-          <div class="text ellipsis" style="margin-left: 10px; margin-right: 10px">
-            <mu-card-text class="text-concat">
-              我于今日晚上六点十分左右在广州市天河区五山路，华晟大厦对面的天桥底与我家泰迪发发走丢了，它是公狗，没有穿衣服，毛发比较长，颜色均匀，走路的时候喜欢歪着走，体型不大，大概六斤左右，脖子上挂些绳圈但没有系绳子，希望有捡到或者看到过的朋友联系我一下，有酬谢！丢了爱犬很着急，多谢大家了，qq同微信号:309094783
-            </mu-card-text>
-          </div>
-
-          <mu-card-actions>
-            <mu-flex class="flex-wrapper" justify-content="end">
-              <mu-button flat>
-                More
-                <mu-icon value="keyboard_arrow_right"></mu-icon>
-              </mu-button>
-            </mu-flex>
-          </mu-card-actions>
-        </mu-card>
-      </div>
+        <taskCard v-for="task in tasks" :key="task.name" v-bind="task"></taskCard>
+</div>
 
 <div class="footer" style="position: fixed; z-index: 10; left: 0; right: 0; bottom: 0;">
   <mu-bottom-nav :value.sync="shift" shift>
@@ -67,7 +41,24 @@ export default {
     return {
       cookie: getToken(),
       shift: 'movies',
-      open: false
+      open: false,
+      tasks: [
+        {
+          name: 'Chen JY',
+          adr: 'Fudan University',
+          tags: [
+            {
+              text: 'find dog',
+              color: 'success'
+            },
+            {
+              text: 'Shanghai',
+              color: 'info'
+            }
+          ],
+          info: '我于今日晚上六点十分左右在广州市天河区五山路，华晟大厦对面的天桥底与我家泰迪发发走丢了，它是公狗，没有穿衣服，毛发比较长，颜色均匀，走路的时候喜欢歪着走，体型不大，大概六斤左右，脖子上挂些绳圈但没有系绳子，希望有捡到或者看到过的朋友联系我一下，有酬谢！丢了爱犬很着急，多谢大家了，qq同微信号:309094783'
+        }
+      ]
     }
   },
   computed: {
