cac816c0434a7aa9614d697cc2615277e77d2121
窗明 <tanglimpse@chuangmingjijingdeMacBook-Pro.local>
2018-11-13 10:33:20 +0800

    Add two test files for Login.vue and UserInfo.vue.

--- frontend/src/components/UserInfo.vue
+++ frontend/src/components/UserInfo.vue
@@ -1,48 +1,48 @@
 <template>
   <div style="height: 100%; width: 100%; position: 100%; top: 0;">
     <mu-container style="margin-top-40">
-<div class="header" style="position: fixed; z-index: 10; left: 0; right: 0; top: 0;">
-    <mu-appbar style="width: 100%;" color="primary">
-  <mu-button icon slot="left" @click="open = !open">
-    <mu-icon value="menu"></mu-icon>
-  </mu-button >
-  Sicilia
-    <mu-button flat slot="right">
-           <mu-icon value="search"></mu-icon>
-      </mu-button>
-</mu-appbar>
-    <mu-drawer :open.sync="open" :docked="true" :right="false">
-      <mu-list>
+      <div class="header" style="position: fixed; z-index: 10; left: 0; right: 0; top: 0;">
+        <mu-appbar style="width: 100%;" color="primary">
+          <mu-button icon slot="left" @click="open = !open">
+            <mu-icon value="menu"></mu-icon>
+          </mu-button >
+          Sicilia
+          <mu-button flat slot="right">
+            <mu-icon value="search"></mu-icon>
+          </mu-button>
+        </mu-appbar>
+        <mu-drawer :open.sync="open" :docked="true" :right="false">
+          <mu-list>
+              <mu-list-item button>
+            <mu-list-item-content>
+              <mu-list-item-title>{{name}}</mu-list-item-title>
+            </mu-list-item-content>
+          </mu-list-item>
+
           <mu-list-item button>
-        <mu-list-item-content>
-          <mu-list-item-title>{{name}}</mu-list-item-title>
-        </mu-list-item-content>
-      </mu-list-item>
+            <mu-list-item-content>
+              <mu-list-item-title>{{password}}</mu-list-item-title>
+            </mu-list-item-content>
+          </mu-list-item>
 
-      <mu-list-item button>
-        <mu-list-item-content>
-          <mu-list-item-title>{{password}}</mu-list-item-title>
-        </mu-list-item-content>
-      </mu-list-item>
-
-      <mu-list-item button>
-        <mu-list-item-content>
-          <mu-list-item-title>{{cookie}}</mu-list-item-title>
-        </mu-list-item-content>
-      </mu-list-item>
-    </mu-list>
-    </mu-drawer>
-</div>
-<div class="footer" style="position: fixed; z-index: 10; left: 0; right: 0; bottom: 0;">
-  <mu-bottom-nav :value.sync="shift" shift>
-    <mu-bottom-nav-item title="Like"  icon="favorite"></mu-bottom-nav-item>
-    <mu-bottom-nav-item title="Discovery"  icon="schedule"></mu-bottom-nav-item>
-    <mu-bottom-nav-item title="Notification"  icon="bookmark"></mu-bottom-nav-item>
-    <mu-bottom-nav-item title="Setting"  icon="settings"></mu-bottom-nav-item>
-  </mu-bottom-nav>
+          <mu-list-item button>
+            <mu-list-item-content>
+              <mu-list-item-title>{{cookie}}</mu-list-item-title>
+            </mu-list-item-content>
+          </mu-list-item>
+          </mu-list>
+        </mu-drawer>
+      </div>
+      <div class="footer" style="position: fixed; z-index: 10; left: 0; right: 0; bottom: 0;">
+        <mu-bottom-nav :value.sync="shift" shift>
+          <mu-bottom-nav-item title="Like"  icon="favorite"></mu-bottom-nav-item>
+          <mu-bottom-nav-item title="Discovery"  icon="schedule"></mu-bottom-nav-item>
+          <mu-bottom-nav-item title="Notification"  icon="bookmark"></mu-bottom-nav-item>
+          <mu-bottom-nav-item title="Setting"  icon="settings"></mu-bottom-nav-item>
+        </mu-bottom-nav>
+       </div>
+     </mu-container>
   </div>
-</mu-container>
-    </div>
 </template>
 
 <script>
