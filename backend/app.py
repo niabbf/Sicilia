44f459fac26feadf71ab8bd829eb41ed3212ae23
VOIAFBR <VOIAFBR@hotmail.com>
2018-12-15 22:59:28 +0800

    fix cors

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from routes.alive import alive as alive_blueprint
 from routes.test import test as test_blueprint
 from routes.tasks import tasks as tasks_blueprint
 from routes.files import ifiles as files_blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(user_blueprint)
@@ -11,7 +12,7 @@ app.register_blueprint(alive_blueprint)
 app.register_blueprint(test_blueprint)
 app.register_blueprint(tasks_blueprint)
 app.register_blueprint(files_blueprint)
-
+CORS(app, supports_credentials=True)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
