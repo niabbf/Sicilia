013d898d54d61d4567a3152ee14a354224165d55
niabbf <duanlei601@163.com>
2018-12-15 11:56:52 +0800

    LogUp: done (except connection with backend)

--- frontend/src/components/LogUp.vue
+++ frontend/src/components/LogUp.vue
@@ -8,18 +8,22 @@
           <mu-text-field v-model="validateForm.username" prop="username"></mu-text-field>
         </mu-form-item>
         <mu-form-item prop="password" label="Password" :rules="passwordRules">
-          <mu-text-field v-model="validateForm.password"></mu-text-field>
+          <mu-text-field v-model="validateForm.password" prop="password" :action-icon="visibility_pwd?'visibility_off':'visibility'" :action-click="() => (visibility_pwd=!visibility_pwd)" :type="visibility_pwd ?'text':'password'"></mu-text-field>
         </mu-form-item>
         <mu-form-item prop="repeatPassword" label="Confirm password" :rules="repeatPasswordRules">
-          <mu-text-field v-model="validateForm.repeatPassword"></mu-text-field>
+          <mu-text-field v-model="validateForm.repeatPassword" prop="password" :action-icon="visibility_rpwd?'visibility_off':'visibility'" :action-click="() => (visibility_rpwd=!visibility_rpwd)" :type="visibility_rpwd?'text':'password'"></mu-text-field>
         </mu-form-item>
-        <mu-form-item style="margin-left:25%">
-          <mu-button color="primary" @click="submit">提交</mu-button>
-          <div style="width:5%"></div>
-          <mu-button @click="clear">重置</mu-button>
+        <mu-form-item prop="isAgree" :rules="agreeRules">
+          <mu-checkbox label="CJY is a dog" v-model="validateForm.isAgree"></mu-checkbox>
+        </mu-form-item>
+        <mu-form-item style="width:100%;">
+          <div style="position:absolute;left:50%;transform:translateX(-50%);">
+            <mu-button color="primary" @click="submit">Log Up</mu-button>
+            <mu-button @click="clear">Reset</mu-button>
+          </div>
         </mu-form-item>
       </mu-form>
-      <mu-button flat color="primary">
+      <mu-button flat color="primary" @click="toLogin">
         <mu-icon value="arrow_back"></mu-icon>Back to Login
       </mu-button>
     </mu-container>
@@ -35,15 +39,20 @@ export default {
   data () {
     return {
       usernameRules: [
-        { validate: (val) => !!val, message: '必须填写用户名' }
+        { validate: (val) => !!val, message: 'Username is a must' }
       ],
       passwordRules: [
-        { validate: (val) => !!val, message: '必须输入密码' },
-        { validate: (val) => val.length >= 6, message: '密码长度必须大于等于6' }
+        { validate: (val) => !!val, message: 'Password is a must' },
+        { validate: (val) => val.length >= 6, message: 'Length of password must not be less than 6' }
       ],
       repeatPasswordRules: [
-        { validate: (val) => val === this.validateForm.password, message: '与密码不符' }
+        { validate: (val) => val === this.validateForm.password, message: 'Different from password' }
       ],
+      agreeRules: [
+        { validate: (val) => !!val, message: 'Agreement is a must' }
+      ],
+      visibility_pwd: false,
+      visibility_rpwd: false,
       showDialog: false,
       dialogText: '',
       validateForm: {
@@ -61,7 +70,22 @@ export default {
     ...mapActions('user', [
       'logUp'
     ]),
-    toLogin: function () {
+    submit () {
+      this.$refs.form.validate().then((result) => {
+        console.log('form valied: ', result)
+      })
+    },
+    clear () {
+      this.$refs.form.clear();
+      this.validateForm = {
+        username: '',
+        password: '',
+        repeatPassword: '',
+        visibility: false,
+        isAgree: false
+      }
+    },
+    toLogin () {
       this.$router.push({ path: '/login' })
     }
   }
