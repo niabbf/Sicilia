f6fdaee7d048c7622a0c60e2969d2286ad05af63
VOIAFBR <VOIAFBR@hotmail.com>
2018-12-22 22:36:57 +0800

    [backend] add tasks api

--- backend/routes/tasks.py
+++ backend/routes/tasks.py
@@ -1,39 +1,121 @@
 from flask import Blueprint, request
 from common.auth import auth
 from pymongo import MongoClient
+import uuid
 import json
 
 tasks = Blueprint('tasks', __name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['SICILIA']
 Task = db['Tasks']
+Account = db['Account']
 
 
-@tasks.route('/owntasks', methods=['GET'])
+def convert_date(value):
+    return value
+
+def convert_post_data_to_query(post_data):
+    return {}
+
+
+@tasks.route('/own_tasks', methods=['POST'])
 @auth
 def get_own_tasks():
+    post_data = request.form.to_dict()
+    query_params = convert_post_data_to_query(post_data)
     user = request.headers.get('user')
+    query_params['task_sponser'] = user
     ret = []
-    for task in Task.find({'user': user}):
+    for task in Task.find(query_params):
         task.pop('_id')
         ret.append(task)
     return json.dumps(ret)
 
 
-@tasks.route('/tasks', methods=['GET'])
+@tasks.route('/executing_tasks', methods=['POST'])
+@auth
+def get_executing_tasks():
+    post_data = request.form.to_dict()
+    query_params = convert_post_data_to_query(post_data)
+    user = request.headers.get('user')
+    query_params['task_executor'] = user
+    query_params['status'] = 1
+    ret = []
+    for task in Task.find(query_params):
+        task.pop('_id')
+        ret.append(task)
+    return json.dumps(ret)
+
+
+@tasks.route('/tasks', methods=['POST'])
 @auth
 def get_tasks():
+    post_data = request.form.to_dict()
+    query_params = convert_post_data_to_query(post_data)
+    query_params['status'] = 0
     ret = []
-    for task in Task.find({}):
+    for task in Task.find(query_params):
         task.pop('_id')
         ret.append(task)
-    return json.dumps(ret)
+    return json.dumps(ret), 200
 
 
-@tasks.route('/add_task', methods=['POST'])
+@tasks.route('/publish_task', methods=['POST'])
 @auth
-def add_task():
+def publish_task():
     post_data = request.form.to_dict()
-    post_data['user'] = request.headers.get('user')
-    task = Task.insert_one(post_data)
-    return 'success'
+    user = request.headers.get('user')
+    task = {}
+    task['task_sponser'] = user
+    task['deadline'] = convert_date(post_data.get('deadline', '2100/01/01'))
+    task['description'] = post_data.get('description', 'no description')
+    task['location'] = post_data.get('location', 'shanghai')
+    reward = int(post_data.get('reward', 0))
+    task_id = str(uuid.uuid4())
+    task['status'] = 0
+    task['task_id'] = task_id
+    task['task_executor'] = None
+    user_info = Account.find_one({'user': user})
+    if user_info is None or user_info.get('cash', 0) < reward:
+        return 'you do not have enough money to publish this task.', 400
+    task['reward'] = reward
+    Account.update_one({'user': user_info.get('user', '')}, {'$set': {'cash': int(user_info.get('cash', 0)) - reward}})
+    task = Task.insert_one(task)
+    return task_id, 200
+
+
+@tasks.route('/confirm_task/<task_id>', methods=['GET'])
+@auth
+def confirm_task(task_id):
+    user = request.headers.get('user')
+    task = Task.find_one({'task_id': task_id})
+    if task is None:
+        return 'task do not exist', 400
+    if task.get('task_sponser') != user:
+        return 'you are not task sponser', 400
+    print(task)
+    task_executor = task.get('task_executor')
+    if task_executor is None:
+        return 'this task has no executor', 400
+    if task.get('status') != 1:
+        return 'this task is not in executing', 400
+    user_info = Account.find_one({'user': task_executor})
+    if user_info is None:
+        return 'task executor do not exist', 400
+    Account.update_one({'user': task_executor}, {'$set': {'cash': user_info.get('cash', 0) + task.get('reward', 0)}})
+    Task.update_one({'task_id': task_id}, {'$set': {'status': 2}})
+    return 'success', 200
+
+
+@tasks.route('/start_task/<task_id>', methods=['GET'])
+@auth
+def start_task(task_id):
+    user = request.headers.get('user')
+    task = Task.find_one({'task_id': task_id})
+    if task is None:
+        return 'task do not exist', 400
+    task_executor = task.get('task_executor')
+    if task_executor is not None:
+        return 'this task is already on executing', 400
+    Task.update_one({'task_id': task_id}, {'$set': {'task_executor': user, 'status': 1}})
+    return 'success', 200
