8c0526c9e84199df471c96727696f16a27985474
VOIAFBR <VOIAFBR@hotmail.com>
2018-12-27 18:08:58 +0800

    add upload image api

--- backend/routes/files.py
+++ backend/routes/files.py
@@ -1,20 +1,29 @@
 from flask import Blueprint, request
 from common.auth import auth
-import os
+from pymongo import MongoClient
+import json
 
 ifiles = Blueprint('ifiles', __name__)
+client = MongoClient('mongodb://localhost:27017/')
+db = client['SICILIA']
+Image = db['Image']
 
 
-@ifiles.route('/upload', methods=['POST'])
+@ifiles.route('/upload_image', methods=['POST'])
 @auth
 def upload_file():
     post_data = request.form.to_dict()
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    try:
-        img = request.files.get('file')
-        path = basedir + "/../static/images/"
-        file_path = path + img.filename
-        img.save(file_path)
-    except Exception as e:
-        print(e)
-    return 'upload success'
+    image_id = str(uuid.uuid4())
+    image = post_data
+    image['image_id'] = image_id
+    Image.insert_one(image)
+    return image_id
+
+
+@ifiles.route('/download_image', methods=['POST'])
+@auth
+def download_file():
+    post_data = request.form.to_dict()
+    image_id = post_data.get('image_id', '')
+    image = Image.find_one({'image_id': image_id})
+    return json.dumps(image), 200
